29bdbac0acae56655cfd39b259b46f88b421ab07
SourabhKanase <sourabhkanase2175@gmail.com>
2023-11-08 15:30:50 +0530

    airbnb1st

---
+++ script.js
@@ -0,0 +1,19 @@
+let form=document.getElementById("form");
+form.addEventListener("submit",(event)=>{
+   event.preventDefault();
+   let obj={
+    location:event.target.location.value,
+    checkInDate:event.target.checkIn.value,
+    checkOutDate:event.target.checkout.value,
+    NoOfPeople:event.target.Quests.value
+   }
+//    console.log("running");
+   localStorage.setItem("info",JSON.stringify(obj));
+//    console.log(JSON.parse(localStorage.getItem("info")));
+    navigateToSecondPage();
+});
+
+function navigateToSecondPage()
+{
+    window.location.href="secondPage.html";
+}
